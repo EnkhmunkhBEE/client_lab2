53d63e9a61060d91ba7517306694556443326152
EnkhmunkhBEE <jingoat1111@gmail.com>
2026-02-05 10:45:09 +0800

    Update settings.py

--- laboo2/settings.py
+++ laboo2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@8smn)b09kvbfrg6)0$a-ip2tof*8&vux9y*(dt5961h^zw3vy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
